b1ab68327ca1d8c7d55a0b08db98159190c39a1c
MianXinhDep <trinmfx04649@funix.edu.vn>
2022-10-16 13:13:12 +0700

    Finally

--- test.js
+++ test.js
@@ -1,2 +1,36 @@
-var a = 'đặkghakjg'; 
-var b =23;
+/*
+Table_1
+    CateID = Khoá chính
+    Name = Tên
+    Name_K_D = Tên không dấu
+    Status = Trạng thái ẩn hiện
+    Sort = sắp xếp
+    ParentID = nào có = 1 là cấp cha còn 0 là cấp con
+        trường để seo
+    MetaKeyword 
+    Metadescriprtioon
+    CreateBy
+    CreateDate
+    UpdateBy 
+    UpdatDate
+
+*Luu y:
+    CateID là primaryKey set tự độg tăng
+    Status set giá trị là 1
+
+Table_2
+    ProductID = PrimaryKey
+    Name.Name_KD.Status
+    Image = src""
+    ListImage = (anh nho nho..?)
+    Price = Gia niem yet
+    PromotionPrice = Gia khuyen mai
+    Vat = thue
+    Quantify = So luong hang ton kho
+    Warranty = Tg Bảo hành
+    Hot = sp Hot
+    Description = Mô tả sp
+    Detail = sp Chi tiết
+    Viewcount = Số lượng ng xem
+    CateID = khoá ngoại
+*/
